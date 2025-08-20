c10de3cc14d7998f03b4e2ff547a66dfb8473b67
Jana Markovikj <janam@iborn.net>
2025-08-20 12:17:17 +0200

    style change

--- src/components/PropahMain/page.jsx
+++ src/components/PropahMain/page.jsx
@@ -1,14 +1,23 @@
 const Page = ({ section }) => {
   const { title, subtitle } = section;
 
-  return (
-    <section id={title}>
+  if (title == 'art') {
+    return (<section id={title}>
       <div className="content">
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
+        <h1>MY ART</h1>
+        <p>This is my art</p>
       </div>
-    </section>
-  );
+    </section>)
+  } else {
+    return (
+      <section id={title}>
+        <div className="content">
+          <h1>{title}</h1>
+          <p>{subtitle}</p>
+        </div>
+      </section>
+    );
+  }
 };
 
 export default Page;
